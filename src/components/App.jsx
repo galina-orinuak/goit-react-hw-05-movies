1f40b2df51631e23e9201d75d7949a235011e6aa
kruhitka1706 <kruhitka1706@gmail.com>
2023-10-24 16:33:13 +0300

    start commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,20 @@
+// import {  Route, Routes } from 'react-router-dom';
+
+// import { Home } from 'pages/Home';
+// import { Movies } from 'pages/Movies';
+// import { MovieDetails } from 'pages/MovieDetails';
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Home/>} />
+//         <Route path="/movies" element={<Movies/>} />
+//         <Route path="/movies/:movieId" element ={<MovieDetails/>}/>
+//       </Routes>
+//     </div>
+//   )
+// };
 export const App = () => {
   return (
     <div
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
+  );
