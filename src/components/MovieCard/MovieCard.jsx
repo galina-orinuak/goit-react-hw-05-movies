30b562b501326e23524751fb4478811a4b5fdbd1
kruhitka1706 <129176072+kruhitka1706@users.noreply.github.com>
2023-11-06 20:32:01 +0200

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,45 +1,65 @@
-import { useRef } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import { Outlet } from "react-router-dom";
-import { Location } from "react-router-dom";
-import styles from './MovieCard.module.css'
+import { useRef } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+// import { Location } from 'react-router-dom';
+import styles from './MovieCard.module.css';
 
+export const MovieCard = ({ movieInfo }) => {
+  const location = useLocation();
+  const backLInkHref = useRef(location.state?.from ?? '/');
 
-export const MovieCard =({movieInfo})=>{
-  
-const location = useLocation();
-const backLInkHref = useRef(location.state?.from ?? '/')
+  let nameOfMovie = null;
+  if (movieInfo.title) {
+    nameOfMovie = movieInfo.title;
+  } else {
+    nameOfMovie = movieInfo.name;
+  }
 
+  let userScore = Number(movieInfo.vote_average * 10).toFixed(0);
+  let genres = movieInfo.genres.map(genre => genre.name).join(', ');
 
-    
-    let nameOfMovie = null;
-        if (movieInfo.title) {
-          nameOfMovie = movieInfo.title;
-        } else {
-          nameOfMovie = movieInfo.name;
-        }
-
-        let userScore = Number(movieInfo.vote_average * 10).toFixed(0);
-        let genres = movieInfo.genres.map(genre => genre.name).join(', ')
-
-    return <><div>
+  return (
+    <>
+      <div className={styles.movieContainer}>
         <div>
-          <NavLink to={backLInkHref.current}>GO BACK</NavLink></div>
-        <div><img srcSet={`https://image.tmdb.org/t/p/w500${movieInfo.poster_path}`} alt="" /></div>
-        <div>
+          <NavLink to={backLInkHref.current}>GO BACK</NavLink>
+        </div>
+        <div className={styles.movieCard}>
+          <div>
+            <img
+            className={styles.movieCardImg}
+              srcSet={`https://image.tmdb.org/t/p/w500${movieInfo.poster_path}`}
+              alt=""
+            />
+          </div>
+          <div className={styles.movieInfo}>
             <h1>{nameOfMovie}</h1>
             <p>User Score: {userScore}%</p>
             <h2>Overwiev</h2>
             <p>{movieInfo.overwiev}</p>
             <h2>Genres</h2>
             <p>{genres}</p>
-            <div>
-               <h2>Additional information</h2>
-               <NavLink to={`/movies/${movieInfo.id}/cast`} className={styles.cast}>Casts</NavLink>
-               <NavLink to={`/movies/${movieInfo.id}/reviews`} className={styles.rewievs}>Reviews</NavLink>
             </div>
-            <Outlet/>
-        </div>
-    </div>
+            </div>
+            <div>
+              <h2>Additional information</h2>
+              <NavLink
+                to={`/movies/${movieInfo.id}/cast`}
+                className={styles.cast}
+              >
+                Casts
+              </NavLink>
+              <NavLink
+                to={`/movies/${movieInfo.id}/reviews`}
+                className={styles.rewievs}
+              >
+                Reviews
+              </NavLink>
+            </div>
+          
+          <Outlet />
+        
+      </div>
     </>
-}
+  );
+};
